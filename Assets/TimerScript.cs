395c256835e54303590d7b86721fd4c1ecb78abf
Igor Samusev <i.samusiev@pampamstudio.com>
2022-05-20 20:28:59 +0300

    Добавьте файлы проекта.

---
+++ Assets/TimerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    public float MaxTime;
+    public Image img;
+    
+    private float currentTime;
+    void Start()
+    {
+        img = GetComponent<Image>();
+        currentTime = 0;
+    }
+
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        if (currentTime >= MaxTime)
+        {
+            currentTime = 0;
+        }
+
+        img.fillAmount = currentTime / MaxTime;
+    }
+}
